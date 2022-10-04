0042476100d678267ade413f14812a7e1b08547e
Mauricio <Siumauricio@unitec.edu>
2022-10-03 22:28:04 -0600

    init project

---
+++ components/Cell/cell.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Cell = () => {
+   return <div>Cell</div>;
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,37 @@
 import '../styles/globals.css';
+import {createTheme, NextUIProvider} from '@nextui-org/react';
+import {ThemeProvider as NextThemesProvider} from 'next-themes';
 import type {AppProps} from 'next/app';
 
+const lightTheme = createTheme({
+   type: 'light',
+   theme: {
+      colors: {},
+   },
+});
+
+const darkTheme = createTheme({
+   type: 'dark',
+   theme: {
+      colors: {},
+   },
+});
+
 function MyApp({Component, pageProps}: AppProps) {
-   return <Component {...pageProps} />;
+   return (
+      <NextThemesProvider
+         defaultTheme="system"
+         attribute="class"
+         value={{
+            light: lightTheme.className,
+            dark: darkTheme.className,
+         }}
+      >
+         <NextUIProvider>
+            <Component {...pageProps} />
+         </NextUIProvider>
+      </NextThemesProvider>
+   );
 }
 
 export default MyApp;
