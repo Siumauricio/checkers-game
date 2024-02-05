a54f54e0405b0fa3765202c62ab54e547d2b8461
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-02-05 01:06:06 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,7 @@ const darkTheme = createTheme({
 });
 
 function MyApp({Component, pageProps}: AppProps) {
+   asdasdfadsf asdf asdf .asd fasdf asdf
    return (
       <NextThemesProvider
          defaultTheme="system"
